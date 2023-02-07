b5aba428f4c9528a15738e4ee513375ffecfdedd
David Perez <david.ezequiel.pere@gmail.com>
2023-02-06 23:42:17 -0300

    POST method

--- index.js
+++ index.js
@@ -21,8 +21,13 @@ const friends = [
 
 server.on('request', (req, res)=>{
     const items = req.url.split('/')
-
-    if (items[1]  === 'friends') {    
+    if (req.method === 'POST' && items[1]  === 'friends') { 
+        req.on('data', (data) => {
+            const friend = data.toString()
+            console.log('Request data: ', friend)
+            friends.push(JSON.parse(friend))
+        })   
+    } else if (req.method === 'GET' && items[1]  === 'friends') {    
         /* res.writeHead(200,{
         'ContentType': 'application/json',
         }) */
@@ -34,7 +39,7 @@ server.on('request', (req, res)=>{
         } else {
             res.end(JSON.stringify(friends))            
         }
-    } else if (items[1]=== 'messages') {
+    } else if (req.method === 'POST' && items[1]=== 'messages') {
         res.statusCode = 200
         res.setHeader('Content-Type', 'html')
 
