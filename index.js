d2c0b1cb973bc5dc3a90439c899a3e3f295f6d79
David Perez <david.ezequiel.pere@gmail.com>
2023-02-07 10:31:35 -0300

    Response echo implemented using .pipe on request.

--- index.js
+++ index.js
@@ -26,7 +26,19 @@ server.on('request', (req, res)=>{
             const friend = data.toString()
             console.log('Request data: ', friend)
             friends.push(JSON.parse(friend))
-        })   
+        })
+        req.pipe(res) // this echoes the posted data, back to the client.  
+        
+        /* This piece of code is the post message to pass via browser console to simulate the POST with an echo request:
+        
+        fetch('http://localhost:3000/friends', {
+            method: 'POST',
+            body: JSON.stringify({id: 3, name: 'Igor Stravinsky'})
+        })
+        .then((response) => response.json())
+        .then((friend) => console.log(friend))
+        
+        */ 
     } else if (req.method === 'GET' && items[1]  === 'friends') {    
         /* res.writeHead(200,{
         'ContentType': 'application/json',
